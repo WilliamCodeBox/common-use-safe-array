cf8f52bab559d08dad62af15e3c032fd4eac218c
Liu Chang <luroylau@163.com>
2020-04-29 16:39:42 +0800

    basic getter and setter functions

--- include/array.hpp
+++ include/array.hpp
@@ -9,6 +9,9 @@
 namespace cousar {
 template <typename T>
 class Array {
+    //*******************************************************************
+    //* Constructors and Destructors
+    //*******************************************************************
    public:
     Array()
         : m_len(0),
@@ -16,14 +19,24 @@ class Array {
           m_cols(0),
           m_resize_chunk(2),
           m_data(nullptr),
-          m_data_size(0) {}
+          m_capacity(0) {}
     Array(int64_t size)
         : m_len(0),
           m_rows(0),
           m_cols(0),
           m_resize_chunk(2),
           m_data(nullptr),
-          m_data_size(0) {}
+          m_capacity(0) {}
+
+    Array(const Array<T>& arr)
+        : m_len(arr.m_len),
+          m_rows(arr.m_rows),
+          m_cols(arr.m_cols),
+          m_data(nullptr),
+          m_resize_chunk(arr.m_resize_chunk),
+          m_capacity(arr.m_capacity) {
+        copy(arr);
+    }
 
     ~Array() {
         if (m_own_data) {
@@ -32,26 +45,40 @@ class Array {
         }
     }
 
-    void resize(int64_t new_size) {
-        if (new_size > m_data_size) {
-            m_data_size = new_size + m_resize_chunk;
-            try {
-                T* new_data = new T[m_data_size];
-                std::make_unique<T>(new_data);
+    //*******************************************************************
+    //* Getters and Setters
+    //*******************************************************************
+    int64_t size() const { return m_len; }
+    int64_t rows() const { return m_rows; }
+    int64_t cols() const { return m_cols; }
+    int64_t capacity() const { return m_capacity; }
 
+    //*******************************************************************
+    //* Public Member Methods
+    //*******************************************************************
+    void resize(int64_t new_capacity) {
+        if (new_capacity > m_capacity) {
+            m_capacity = new_capacity + m_resize_chunk;
+            try {
+                T* new_data = new T[m_capacity];
                 // copy the original data into the new array
                 if (m_len != 0) {
                     std::memcpy(new_data, m_data, m_len * sizeof(T));
                 }
-                // set to nullptr
+                // deallocate the original memory
+                // free it
                 delete[] m_data;
                 // then point to the new created array
-                m_data = std::move(new_data);
+                m_data = new_data;
+                new_data = nullptr;
             } catch (const std::bad_alloc& e) {
                 throw new Exception("Out of memory!");
             }
+        } else {
+            throw new Exception(
+                "Resize to smaller capacity, could loss original data.")
         }
-        m_len = new_size;
+        m_len = new_capacity;
     }
 
     void resize(int64_t n_rows, int64_t n_cols) {
@@ -60,14 +87,33 @@ class Array {
         resize(m_rows * m_cols);
     }
 
+    //*******************************************************************
+    //* Member Variables
+    //*******************************************************************
    private:
     int64_t m_len;
     int64_t m_rows;
     int64_t m_cols;
     int64_t m_resize_chunk;
 
-    std::unique_ptr<T>* m_data;
-    int64_t m_data_size;
+    T* m_data;
+    int64_t m_capacity;
+
+    //*******************************************************************
+    //* Private Member Methods
+    //*******************************************************************
+    void copy(const Array<T>& arr) {
+        if (arr.m_rows == 0) {
+            resize(arr.m_len);
+        } else {
+            resize(arr.m_rows, arr.m_cols);
+        }
+        m_data = new T[m_len];
+
+        for (int64_t i = 0; i < m_len; i++) {
+            m_data[i] = arr[i];
+        }
+    }
 };
 }  // namespace cousar
 
