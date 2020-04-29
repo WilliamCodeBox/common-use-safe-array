2128e9789d653b1a434ef39e1820d9e4568cb8bf
Liu Chang <luroylau@163.com>
2020-04-29 14:44:10 +0800

    add custom exception class inherited from std::exception

---
+++ include/exception.hpp
@@ -0,0 +1,15 @@
+#if !defined(COUSAR_EXCEPTION_H)
+#define COUSAR_EXCEPTION_H
+
+#include <exception>
+#include <string>
+
+namespace cousar {
+class Exception : public std::exception {
+ public:
+  Exception(const std::string& error);
+  const char* what() const noexcept;
+};
+}  // namespace cousar
+
+#endif  // COUSAR_EXCEPTION_H
